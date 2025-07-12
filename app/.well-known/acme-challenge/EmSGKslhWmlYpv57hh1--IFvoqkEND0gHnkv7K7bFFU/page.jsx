8c279e2a40edb15eb777994537e42a7157384a3e
Anikettha23 <anikett2305@gmail.com>
2025-07-12 11:51:02 +0530

    response 200

---
+++ app/.well-known/acme-challenge/EmSGKslhWmlYpv57hh1--IFvoqkEND0gHnkv7K7bFFU/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      .well-known/acme-challenge/EmSGKslhWmlYpv57hh1--IFvoqkEND0gHnkv7K7bFFU
+    </div>
+  )
+}
+
+export default page
