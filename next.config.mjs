92c9b99ea6c1b849d3f3b2e4a79f3a2f712a79a9
AmanKumar00002 <aman.kumar@ireedindia.com>
2025-07-17 11:49:58 +0530

    asdfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'svsconstructions.com',
-          },
-        ],
-        destination: 'https://www.svsconstructions.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
+
 
 export default nextConfig;
-
