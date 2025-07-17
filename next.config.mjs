926dc2a58b89c2fe39ea35f8750047fd890a1335
AmanKumar00002 <aman.kumar@ireedindia.com>
2025-07-17 11:30:36 +0530

    rederction

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.svsconstructions.com',
+          },
+        ],
+        destination: 'https://svsconstructions.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
